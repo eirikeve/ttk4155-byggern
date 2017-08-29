1d5b045d6ed153b88ac01851c7618a3cae3865d5
aasmune <aasm.eek@gmail.com>
2017-08-29 20:01:55 +0200

    Added dummy test to check if unity config works with drone

---
+++ node_1/node_1.h
@@ -0,0 +1,10 @@
+#ifndef NODE_1_H
+#define NODE_1_H
+ 
+#include <stdint.h>
+#include <stdbool.h>
+ 
+int8_t AverageThreeBytes(int8_t a, int8_t b, int8_t c);
+// bool foo(void);
+ 
+#endif /* NODE_1_H */
