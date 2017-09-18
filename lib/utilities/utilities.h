078223c433e51aba28da0917b518113253bca69c
Eirik Vesterkjær <eve.eirik@gmail.com>
2017-09-18 11:44:44 +0200

    Added bit-functions to utilities

--- lib/utilities/utilities.h
+++ lib/utilities/utilities.h
@@ -4,3 +4,11 @@
 // #define F_CPU 4915200
 
 // #endif // UTILITIES_H
+
+#pragma once
+
+#define set_bit(reg, bit) (reg |= (1 << (bit)))
+#define set_bit_value(reg, bit, val) (reg |= (val << (bit)))
+#define clr_bit(reg, bit) (reg &= ~(1 << (bit)))
+#define test_bit(reg, bit) (reg & (1 << (bit)))
+#define set_byte(reg, val) (reg = val)
