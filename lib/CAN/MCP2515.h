25b8acb7ada9a3bb0d0d7133e519a1364d9fda33
Eirik Vesterkjær <eve.eirik@gmail.com>
2017-10-09 14:16:02 +0200

    Interrupt vector enabling

--- lib/CAN/MCP2515.h
+++ lib/CAN/MCP2515.h
@@ -1,4 +1,9 @@
 #pragma once
+#include "../utilities/utilities.h"
+extern "C" {
+    #include <avr/interrupt.h>
+}
+
 
 uint8_t mcp2515_init();
 uint8_t mcp2515_read(uint8_t address);
@@ -7,3 +12,9 @@ void mcp2515_request_to_send();
 void mcp2515_bit_modify();
 void mcp2515_reset();
 void mcp_read_status();
+
+ISR(INT1_vect)
+{
+    if ()
+    // Interrupt handler for SPI
+}
