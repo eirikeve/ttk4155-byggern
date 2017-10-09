eaa4220193fb54ccf61b3de4fd3610ea9d3f3556
Eirik Vesterkjær <eve.eirik@gmail.com>
2017-10-09 14:16:31 +0200

    Setting SS' to output, sending and receiving

--- lib/CAN/SPI.cpp
+++ lib/CAN/SPI.cpp
@@ -1,19 +1,35 @@
 #pragma once
+#include "../utilities/utilities.h"
 
  SPI_init(void){
-	/* Set MOSI and SCK output, all others input */
-	DDR_SPI = (1<<DD_MOSI)|(1<<DD_SCK);
+
+	/* Set 		 SS' 	  MOSI and 	 SCK to output, all others input */
+	DDRB = (1 <<DDB4)|(1<<DDB5)|(1<<DDB7);
 	/* Enable SPI, Master, set clock rate fck/16 */
 	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+
 }
 
 void SPI_send(char cData){
 	/* Start transmission */
 	SPDR = cData;
 	/* Wait for transmission complete */
-	while(!(SPSR & (1<<SPIF)));
+	//while(!(SPSR & (1<<SPIF))); // Changed to the line below
+	loop_until_bit_is_set(SPSR, SPIF);
 }
 
-void SPI_read(){
-	
+void SPI_send_string(char * c_string)
+{
+	uint8_t index = 0;
+	while (c_string[index] != '\0')
+	{
+		SPI_send(c_string[index])
+		index += 1;
+		if (index == UINT8_MAX) return; // To avoid infinite loop
+	}
+}
+
+char SPI_read(){
+	SPI_send(0x00);
+	return SPDR;
 }
