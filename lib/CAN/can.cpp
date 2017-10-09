21894e7d985c265103b3f89512c851830ec647a7
Eirik Vesterkjær <eve.eirik@gmail.com>
2017-10-09 14:35:14 +0200

    Commented out main loop

--- lib/CAN/can.cpp
+++ lib/CAN/can.cpp
@@ -2,20 +2,21 @@
 #include "SPI.h"
 #include "MCP2515.h"
 
+
 struct can_message{
 	unsigned int id;
 	uint8_t length;
 	uint8_t data[8];
 };
 
-int main(void){
-	can_init();
-	struct can_message message;
-	message.id = 3;
-	message.length = 1;
-	message.data[0] = (uint8_t)'U';
-	can_message_send(&message);
-}
+// int main(void){
+// 	can_init();
+// 	struct can_message message;
+// 	message.id = 3;
+// 	message.length = 1;
+// 	message.data[0] = (uint8_t)'U';
+// 	can_message_send(&message);
+// }
 
 void can_message_send(struct can_message* msg){
 	//...
