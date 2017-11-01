de8a70af549eea1c11c5f05ce22c8f4828cee6d5
Åsmund Eek <aasm.eek@gmail.com>
2017-11-01 22:38:26 +0100

    Removed commented out old code

--- lib/uart/uart.h
+++ lib/uart/uart.h
@@ -1,39 +1,11 @@
-// /*
-// * utils.h
-// *
-// * Created: 04.09.2017 11:29:32
-// *  Author: eirikeve
-// */
-
-// #pragma once
-
-// class UART{
-// private:
-//     volatile static UART * instance;
-//     volatile static int8_t num;
-
-//     UART();
-//     static int init();
-//     static unsigned char _recv_char();
-//     static void _flush();
-
-// public:
-
-//     ~UART();
-
-//     static int put_char(char c);
-
-//     static unsigned char recv_char();
-
-//     volatile static UART * instance();
-
-// };
-
 #pragma once
+
 #include <stdint.h>
 #include <stdio.h>
 
-
+/**
+ * Class for communication over UART for AVR atmega162 and atmega2560
+ * */
 class UART
 {
     public:
