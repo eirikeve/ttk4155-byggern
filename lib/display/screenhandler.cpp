691db009e44bd8ae90b5971f54a28dadb874353a
Eirik Vesterkjær <eve.eirik@gmail.com>
2017-11-06 13:20:32 +0100

    Added handler for screen class

---
+++ lib/display/screenhandler.cpp
@@ -0,0 +1,186 @@
+// screenhandler.cpp
+// 6/11/2017
+
+#include "screenhandler.h"
+
+ScreenHandler::ScreenHandler()
+{
+    handler = this; // Flag that an instance exists
+    num_screens = 0;
+    array_size = 5;
+    screens = (Screen**)calloc(array_size, sizeof(Screen*));
+    currentBuffer = (uint8_t*)AVR_VRAM_1;
+}
+void ScreenHandler::render()
+{
+    if (num_screens > 0)
+    {
+        screens[0]->render(); // Renders the whole screen!
+    }
+}
+
+void ScreenHandler::increaseArraySize()
+{
+    int new_size = array_size * 2;
+    Screen  ** new_screens = (Screen**)calloc(new_size, sizeof(Screen*));
+    for (uint8_t i = 0; i < num_screens; ++i)
+    {
+        new_screens[i] = screens[i];
+    }
+    // Might be a bug here! Need to check.
+    free(screens);
+    screens = new_screens;
+    array_size = new_size;
+    
+}
+
+void ScreenHandler::addScreenToArray(Screen * s)
+{
+    if (num_screens >= array_size)
+    {
+        increaseArraySize();
+    }
+    screens[num_screens] = s;
+    ++num_screens;
+}
+
+void ScreenHandler::_changeVRAMBuffer()
+{
+    if ((uint8_t*)AVR_VRAM_1 == currentBuffer)
+    {
+        currentBuffer = (uint8_t*)AVR_VRAM_2;
+    }
+    else
+    {
+        currentBuffer = (uint8_t*)AVR_VRAM_1;
+    }
+    for (uint8_t i = 0; i < num_screens; ++i)
+    {
+        screens[i]->changeBufferTo(currentBuffer);
+        if (i == 0)
+        {
+            screens[0]->copyVRAMtoCurrentBuffer();
+        }
+    }
+    
+}
+
+bool ScreenHandler::_isReadyToRender()
+{
+    for (uint8_t i = 0; i < num_screens; ++i)
+    {
+        if (screens[i]->ready_to_render)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void ScreenHandler::_clearRenderFlags()
+{
+    for (uint8_t i = 0; i < num_screens; ++i)
+    {
+        screens[i]->ready_to_render = false;
+    }
+}
+
+ScreenHandler ScreenHandler::getInstance()
+{
+    if (handler)
+    {
+        return *handler;
+    }
+    else
+    {
+        ScreenHandler h = ScreenHandler();
+        return h;
+    }
+}
+void ScreenHandler::addMainScreen(Screen * s)
+{
+    addScreenToArray(s);
+}
+Screen ScreenHandler::createMainScreen()
+{
+    Screen s;
+    addScreenToArray(&s);
+    return s;
+}
+
+void ScreenHandler::addSubScreen(Screen * superscreen, Screen *subscreen, uint8_t sz, Orientation o)
+{
+    // Check if superscreen is in the handler. If not, do nothing!
+    for (uint8_t i = 0; i < num_screens; ++i)
+    {
+        if (screens[i] == superscreen)
+        {
+            superscreen->addSubScreen(subscreen, sz, o);
+            addScreenToArray(subscreen);
+            return;
+        }
+    }
+}
+Screen ScreenHandler::createSubScreen(Screen *superscreen, uint8_t sz, Orientation o)
+{
+        // Check if superscreen is in the handler. If not, do nothing!
+        for (uint8_t i = 0; i < num_screens; ++i)
+        {
+            if (screens[i] == superscreen)
+            {
+                Screen subscreen = Screen(superscreen, sz, o);
+                addScreenToArray(&subscreen);
+                return subscreen;
+            }
+        }
+}
+void ScreenHandler::removeScreen(Screen * s)
+{
+    if (s->hasSubScreen()){
+        removeScreen(s->subScreen);
+    }
+
+    for (uint8_t i = 0; i < num_screens; ++i)
+    {
+        if (screens[i] == s)
+        {
+            s->~Screen();
+            for (int k = i; k < num_screens; ++k)
+            {
+                if (k == num_screens - 1)
+                {
+                    screens[k] = NULL;
+                }
+                else
+                {
+                    screens[k] = screens[k+1];
+                }
+            }
+            num_screens--;
+            return;
+        }
+    }
+}
+
+uint8_t ScreenHandler::getNumScreens() const
+{
+    return num_screens;
+}
+Screen * ScreenHandler::getScreenPtr(uint8_t screen_index)
+{
+    if (screen_index < num_screens && screen_index >= 0)
+    {
+        return screens[screen_index];
+    }
+    return NULL;
+}
+
+
+void ScreenHandler::interruptHandlerRoutine()
+{
+    if (_isReadyToRender()){
+        _changeVRAMBuffer();
+        render();
+        _clearRenderFlags();
+    }
+}
---
+++ lib/display/screenhandler.h
@@ -0,0 +1,45 @@
+// screenhandler.h
+// 6/11/2017
+#ifndef SCREENHANDLER_H
+#define SCREENHANDLER_H
+#include "screen.h"
+
+class ScreenHandler
+{
+// Private variables
+private:
+    static ScreenHandler * handler;
+    uint8_t num_screens; // Number of screens in handler
+    uint8_t array_size;
+    uint8_t* currentBuffer;
+    Screen ** screens;
+
+// Public variables
+public:
+
+// Private methods
+private:
+    ScreenHandler();
+    void render();
+    void increaseArraySize();
+    void addScreenToArray(Screen * s);
+    void _changeVRAMBuffer();
+    bool _isReadyToRender();
+    void _clearRenderFlags();
+
+// Public methods
+public:
+    static ScreenHandler getInstance();
+    void addMainScreen(Screen * s);
+    Screen createMainScreen();
+    void addSubScreen(Screen * superscreen, Screen *subscreen, uint8_t sz, Orientation o);
+    Screen createSubScreen(Screen *superscreen, uint8_t sz, Orientation o);
+    void removeScreen(Screen * s);
+    uint8_t getNumScreens() const;
+    Screen* getScreenPtr(uint8_t screen_index);
+
+    void interruptHandlerRoutine();
+
+};
+
+#endif
