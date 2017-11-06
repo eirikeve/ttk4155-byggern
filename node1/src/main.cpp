15c57cdeebcde4b98fcdbd150e7460aa38a75ea1
Eirik Vesterkjær <eve.eirik@gmail.com>
2017-11-06 09:52:54 +0100

    Missed main.cpp merge fix

--- node1/src/main.cpp
+++ node1/src/main.cpp
@@ -103,39 +103,6 @@ int main(void)
 
 	testTimer();
 	set_bit(DDRB, 0);
-<<<<<<< HEAD
-	clr_bit(DDRE, 0);
-	init_uart();
-
-	// Joystick joystick(10);
-	uint8_t x;
-	uint8_t y;
-
-	int std_wait_time = 5000;
-	put_uart_char('B');
-	printf("Test\n");
-	put_uart_char('A');
-	x  = 0;
-	y  = 0;
-	Screen bar;
-	bar.clear();
-	bar.writeString("Hello World, this is a test of the display. Does it wrap? \nDoes it newline? Does it work?");
-	bar.writeString(" This is a new string. It is pretty nice. String string strange thing bling bling.");
-	bar.render();
-
-	
-
-	// sub.clear();
-	// _delay_ms(1000);
-	// o.writeString("123456789123456 abcdefghijklmnopqrstuvwxyz ja123456789123456 abcdefghijklmnopqrstuvwxyz ja123456789123456 abcdefghijklmnopqrstuvwxyz ja123456789123456 abcdefghijklmnopqrstuvwxyz ja123456789123456 abcdefghijklmnopqrstuvwxyz ja");
-	// o.goTo(0, 9);
-	// o.writeChar('B');
-	// const unsigned char PROGMEM a[8] = {0b00100000, 0b01110100, 0b01010100, 0b01010100, 0b00111100, 0b01111000, 0b01000000, 0b00000000};
-	// o.gotoPage(0);
-	// for (int i = 0; i < 8; i++)
-	// {
-	// 	o.write(pgm_read_word(&font8[33][i]));
-=======
 	set_bit(PORTB, 0);
 	// init_timer(500);
 
@@ -148,7 +115,6 @@ int main(void)
 	// Joystick joystick(10);
 	int8_t x;
 	int8_t y;
->>>>>>> master
 	// }
 	while (1)
 	{
