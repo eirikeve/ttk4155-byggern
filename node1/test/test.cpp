39d6af75be8b81d0d8571ff37b63d6f943ae2e4b
Eirik Vesterkjær <eve.eirik@gmail.com>
2017-11-12 15:19:29 +0100

    ALE enable

--- node1/test/test.cpp
+++ node1/test/test.cpp
@@ -408,6 +408,8 @@ void SRAM_test()
     uart.initialize(9600);
     enablePrintfWithUart();
 
+    set_bit(DDRE, DDE1);
+
     volatile char *ext_ram = (char *) AVR_VRAM_1; // Start address for the SRAM
     uint16_t ext_ram_size = 0x800;
     uint16_t write_errors = 0;
