520794cccf7bfec56bce596b5a664ae6328da95a
Eirik Vesterkjær <eve.eirik@gmail.com>
2017-11-17 21:56:42 +0100

    Minor change to screenhandler. Merged display_sram into finished_game

---
+++ lib/display/screenhandler.h
@@ -0,0 +1,59 @@
+
+// screenhandler.h
+// 6/11/2017
+#ifndef SCREENHANDLER_H
+#define SCREENHANDLER_H
+#include "screen.h"
+#include "../timer/timer.h"
+
+class ScreenHandler
+{
+// Private variables
+private:
+    const uint8_t max_num_screens = 5; // Number of screens in handler
+    uint8_t num_screens;
+    uint8_t array_size;
+    uint8_t* currentBuffer;
+    Screen * screens[5];
+
+// Public variables
+public:
+
+// Private methods
+private:
+    ScreenHandler();
+    
+    void _render();
+    void _addScreenToArray(Screen * s);
+    void _changeVRAMBuffer();
+    void _clearRenderFlags();
+
+// Public methods
+public:
+    static ScreenHandler& getInstance()
+    {
+        static ScreenHandler instance;
+        return instance;
+    }
+    
+
+    void addScreen(Screen * s);
+    void removeScreen(Screen * s);
+    uint8_t getNumScreens() const;
+    Screen* getScreenPtr(uint8_t screen_index);
+    bool isReadyToRender();
+    
+    void _interruptHandlerRoutine();
+
+    // Deleted due to singleton design pattern
+    ScreenHandler(ScreenHandler const&)    = delete;
+    
+    // Deleted due to singleton design pattern
+    void operator=(ScreenHandler const&)  = delete;
+};
+
+void ScreenHandlerTimerInterrupt();
+
+
+
+#endif
