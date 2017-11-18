aa8d4a897e2439f913953fd9ae97d7ad9151f16b
Eirik Vesterkjær <eve.eirik@gmail.com>
2017-11-18 14:28:16 +0100

    Bit shift bug fix

--- lib/utilities/eeprom.h
+++ lib/utilities/eeprom.h
@@ -6,7 +6,7 @@
 uint8_t eepromRead(uint16_t eeprom_address)
 {
     EEARL = (uint8_t)(eeprom_address & 0xFF);   // Write address
-    EEARH |= (uint8_t)((eeprom_address > 8) & 0x01);
+    EEARH |= (uint8_t)((eeprom_address >> 8) & 0x01);
 
     EECR |= 0b0001;  // Read enable
     return (uint8_t)EEDR; // Data available instantly
@@ -18,7 +18,7 @@ void eepromWrite(uint16_t eeprom_address, uint8_t data)
 {
     loop_until_bit_is_clear(EECR, EEWE);        // Wait until old write is finished
     EEARL = (uint8_t)(eeprom_address & 0xFF);   // Write address and data
-    EEARH |= (uint8_t)((eeprom_address > 8) & 0x01);
+    EEARH |= (uint8_t)((eeprom_address >> 8) & 0x01);
     EEDR = data;
 
     EECR = 0b0100  // Master write enable, while setting write enable to 0 (must be done this way)
