0e74f9be728c3a4e6a8393a08a5e349d7d2f38fa
Eirik Vesterkjær <eve.eirik@gmail.com>
2017-11-18 12:20:37 +0100

    Change in include of pin

--- node1/src/main.cpp
+++ node1/src/main.cpp
@@ -99,7 +99,7 @@
 
 #include "test.h"
 #include "lib/utilities/printf.h"
-#include "lib/utilities/pin.h"
+#include "lib/pins/pins.h"
 #include "lib/uart/uart.h"
 #include "lib/adc/adc.h"
 #include "lib/joystick/joystick.h"
