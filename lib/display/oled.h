e0c41a2545436658c41586fa31fe013d4f750b37
Eirik Vesterkjær <eve.eirik@gmail.com>
2017-11-19 10:58:15 +0100

    Rewriting OLED to singleton

--- lib/display/oled.h
+++ lib/display/oled.h
@@ -5,12 +5,24 @@ extern "C" {
 
 class OLED
 {
-  private:
-    volatile uint8_t *dataAddr;
-    volatile uint8_t *commandAddr;
+
+
+private:
+    const uint8_t* dataAddr = (uint8_t *)0x1200;
+    const uint8_t* commandAddr = (uint8_t *)0x1000;
+    OLED();
 
   public:
-    OLED();
+        static OLED& getInstance()
+    {
+        static OLED instance;
+        return instance;
+    }
+    // Deleted due to singleton design pattern
+    OLED(OLED const&) = delete;
+    // Deleted due to singleton design pattern
+    void operator=(OLED const&) = delete;
+
     void write_c(uint8_t command);
     void write(uint8_t c);
 
