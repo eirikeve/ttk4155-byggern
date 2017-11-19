ec91ae4f6dd9b91c18c6d4414a47565e5d1046e7
aasmune <aasm.eek@gmail.com>
2017-11-19 12:17:46 +0100

    Revert "OLED addr fix"
    This reverts commit 20b005ff2f124f2dd1f77b87401286dfaef31bf3.

--- lib/display/oled.h
+++ lib/display/oled.h
@@ -8,8 +8,8 @@ class OLED
 
 
 private:
-    uint8_t* dataAddr = (uint8_t *)0x1200;
-    uint8_t* commandAddr = (uint8_t *)0x1000;
+    const uint8_t* dataAddr = (uint8_t *)0x1200;
+    const uint8_t* commandAddr = (uint8_t *)0x1000;
     OLED();
 
   public:
