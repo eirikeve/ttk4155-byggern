894a56452f5b388acbd99e09f03e5336f4a84af1
Howiezi <havard.borge.95@gmail.com>
2017-11-20 10:53:10 +0100

    Deleted unused folders

--- lib/example/example.h
+++
@@ -1,9 +0,0 @@
-// #ifndef EXAMPLE_H
-// #define EXAMPLE_H
- 
-// #include <stdint.h>
-// #include <stdbool.h>
- 
-// int8_t AverageThreeBytes(int8_t a, int8_t b, int8_t c);
- 
-// #endif /* EXAMPLE_H */
