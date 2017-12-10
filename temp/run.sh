010d0fee3079797bbf9a2f0e472d0d227bd66c36
Åsmund Eek <aasm.eek@gmail.com>
2017-12-10 13:00:38 +0100

    Added extra feature ble spi service for nRF as submodule, removed unused Unity submodule

--- temp/run.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-
-avr-gcc -mmcu=atmega162 -Os *.c -o main.o -std=c11
-avr-objcopy -j .text -j .data -O ihex main.o main.hex
-avrdude -p atmega162 -c atmelice -U flash:w:main.hex:i -U "lfuse:w:0xfd:m hfuse:w:0x91:m efuse:w:0xff:m"
